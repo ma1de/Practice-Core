b56a50e3d47c0cd7ca7496da03f9a805347882ab
ma1de <ilyabuhtoyarov@protonmail.ch>
2023-12-04 16:33:23 +0500

    oop

--- src/main/java/me/ma1de/practice/gui/Gui.java
+++ src/main/java/me/ma1de/practice/gui/Gui.java
@@ -2,11 +2,6 @@ package me.ma1de.practice.gui;
 
 import java.util.List;
 
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
-import lombok.SneakyThrows;
-
 public interface Gui {
     String getId();
     String getDisplayName();
@@ -14,14 +9,5 @@ public interface Gui {
     List<GuiButton> getButtons();
     
     boolean isEnabled();
-
-    @SneakyThrows
-    default void open(Player player) {
-        if (getClass().getField("inv") == null) {
-            throw new Exception("Unable to find field \"inv\"");
-        }
-
-        Inventory inv = getClass().getField("inv").get(getClass());
-    }
 }
 
