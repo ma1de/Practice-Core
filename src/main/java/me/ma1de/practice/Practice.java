772b83d88811d2a5c70ea84d2f7eb0db1c8f3c3d
ma1de <ilyabuhtoyarov@protonmail.ch>
2024-08-05 16:37:56 +0500

    Start over!

--- src/main/java/me/ma1de/practice/Practice.java
+++ src/main/java/me/ma1de/practice/Practice.java
@@ -1,26 +1,21 @@
 package me.ma1de.practice;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
+@Getter(AccessLevel.PUBLIC)
+public class Practice extends JavaPlugin {
+    @Getter(AccessLevel.PUBLIC)
+    private static Practice instance; // gson is weird
 
-import lombok.Getter;
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
 
-@Getter
-public enum Practice {
-    INSTANCE;
-
-    private final Gson gson = new GsonBuilder()
-        .setPrettyPrinting().create();
-
-    private MongoClient client;
-    private MongoDatabase database;
-
-    public void load(JavaPlugin plugin) {
-        client = new MongoClient(plugin.getConfig().getString("MONGO.HOST"), plugin.getConfig().getInt("MONGO.PORT"));
-        database = client.getDatabase(plugin.getConfig().getString("MONGO.DATABASE"));
+    @Override
+    public void onDisable() {
+        instance = null;
     }
 }
